10c23fbcbdf2c5bbb08122670220561931c06492
Jittapan Pluemsumran <foreverfk@stephack.com>
2016-04-11 17:00:35 +0700

    Initial Commit

---
+++ Patcher/ConfigParser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using YamlDotNet.RepresentationModel;
+
+namespace Patcher
+{
+    public class ConfigParser
+    {
+        string _configFilePath = string.Empty;
+        public ConfigParser(string configFilePath)
+        {
+            _configFilePath = configFilePath;
+        }
+
+        public Config readConfig()
+        {
+            var input = new StringReader(File.ReadAllText(_configFilePath));
+
+            var yaml = new YamlStream();
+            yaml.Load(input);
+
+            try
+            {
+                var config = new Config();
+                var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+                var yamlConfig = (YamlMappingNode)mapping.Children[new YamlScalarNode("config")];
+                var yamlPatchList = (YamlSequenceNode)yamlConfig.Children[new YamlScalarNode("patch_list")];
+                config.patchType = yamlConfig.Children[new YamlScalarNode("patch_type")].ToString();
+                foreach (YamlMappingNode patchList in yamlPatchList)
+                {
+                    config.patchList.Add(new Config.patch_list
+                    {
+                        patchFTPHost = patchList.Children[new YamlScalarNode("patch_ftp_host")].ToString(),
+                        patchListURL = patchList.Children[new YamlScalarNode("patch_list_url")].ToString(),
+                    });
+                }
+
+                return config;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public class Config
+        {
+            public string patchType = string.Empty;
+            public List<patch_list> patchList;
+            
+            public Config()
+            {
+                patchList = new List<Config.patch_list>();
+            }
+
+            public class patch_list
+            {
+                private string _patchListURL;
+                public string patchListURL
+                {
+                    get { return _patchListURL; }
+                    set { _patchListURL = value; }
+                }
+
+                private string _patchFTPHost;
+                public string patchFTPHost
+                {
+                    get { return _patchFTPHost; }
+                    set { _patchFTPHost = value; }
+                }
+            }
+        }
+    }
+}
---
+++ Patcher/VersionParser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+
+namespace Patcher
+{
+    public class VersionParser
+    {
+        string _filePath = string.Empty;
+
+        public VersionParser(string filePath)
+        {
+            _filePath = filePath;
+            if (!File.Exists(_filePath)) // If not exists, create
+            {
+                File.Create(_filePath).Dispose();
+                writeVersion(1); // Write default version (1)
+            }
+        }
+
+        public int readVersion()
+        {
+            BinaryReader reader = new BinaryReader(new FileStream(_filePath, FileMode.Open, FileAccess.Read, FileShare.None));
+            reader.BaseStream.Position = 0x0;     // The offset you are reading the data from
+            byte[] bytes = reader.ReadBytes(0x04); // Read 4 bytes into an array
+            reader.Close();
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        public bool writeVersion(int version)
+        {
+            BinaryWriter Writer = null;
+
+            try {
+                // Create a new stream to write to the file
+                Writer = new BinaryWriter(File.OpenWrite(_filePath));
+
+                // Writer raw data                
+                Writer.Write(BitConverter.GetBytes(version));
+                Writer.Flush();
+                Writer.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
