a86497395fc592451e236f769a39a5d2a0adbf73
NilDigit <nildigit@gmail.com>
2016-04-11 18:12:14 +0700

    + Network class + PatchParser class

---
+++ Patcher/Network.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Patcher
+{
+    class Network
+    {
+        public static Task<string> MakeAsyncRequest(string url, string contentType)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = contentType;
+            request.Method = WebRequestMethods.Http.Get;
+            request.Timeout = 20000;
+            request.Proxy = null;
+
+            Task<WebResponse> task = Task.Factory.FromAsync(
+                request.BeginGetResponse,
+                asyncResult => request.EndGetResponse(asyncResult),
+                (object)null);
+
+            return task.ContinueWith(t => ReadStreamFromResponse(t.Result));
+        }
+
+        private static string ReadStreamFromResponse(WebResponse response)
+        {
+            using (Stream responseStream = response.GetResponseStream())
+            using (StreamReader sr = new StreamReader(responseStream))
+            {
+                //Need to return this response 
+                string strContent = sr.ReadToEnd();
+                return strContent;
+            }
+        }
+    }
+}
---
+++ Patcher/PatchParser.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using System.Text.RegularExpressions;
+
+namespace Patcher
+{
+    public class PatchParser
+    {
+        private Gravity _gravity;
+        public Gravity gravity
+        {
+            get { return _gravity; }
+            set { _gravity = value; }
+        }
+
+        private string _gravityPatchFileListURL;
+
+        public PatchParser(string gravityPatchFileListURL)
+        {
+            _gravityPatchFileListURL = gravityPatchFileListURL;
+            _gravity = new Gravity();
+        }
+
+        public PatchParser(string gravityPatchFileListURL, string junoYamlFileURL)
+        {
+            _gravityPatchFileListURL = gravityPatchFileListURL;
+            _gravity = new Gravity();
+        }
+
+        public bool loadPatchData()
+        {
+            var task = Network.MakeAsyncRequest(_gravityPatchFileListURL, "text/html");
+            string input = task.Result;
+
+            string pattern = @"(([0-9]+)[\t ]+(.+)?$|(\/\/)([0-9]+)[\t ]+(.+)?$)";
+
+            MatchCollection matches = Regex.Matches(input, pattern, RegexOptions.Multiline);
+
+            foreach (Match match in matches)
+            {
+                try
+                {
+                    if (match.Groups[4].Value == @"//")
+                    {
+                        _gravity.patchListIgnore.Add(new Gravity.Patch
+                        {
+                            patchNumber = int.Parse(match.Groups[5].Value),
+                            patchFileName = match.Groups[6].Value,
+                        });
+                    }
+                    else
+                    {
+                        _gravity.patchList.Add(new Gravity.Patch
+                        {
+                            patchNumber = int.Parse(match.Groups[2].Value),
+                            patchFileName = match.Groups[3].Value,
+                        });
+                    }
+                }
+                catch { return false; }
+            }
+            return true;
+        }
+        public class Gravity
+        {
+            public List<Patch> patchList, patchListIgnore;
+
+            public Gravity()
+            {
+                patchList = new List<Patch>();
+                patchListIgnore = new List<Patch>();
+            }
+
+            public class Patch
+            {
+                private int _patchNumber;
+                public int patchNumber
+                {
+                    get { return _patchNumber; }
+                    set { _patchNumber = value; }
+                }
+
+                private string _patchFileName;
+                public string patchFileName
+                {
+                    get { return _patchFileName; }
+                    set { _patchFileName = value; }
+                }
+            }
+        }
+    }
+}
